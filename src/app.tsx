c7f7d71ccbbb6acffdfe42009808eb1f41f963c0
Kevin Franklin Alvarado Cornejo <kfalvara@epichincha.com>
2023-04-27 18:45:21 -0500

    avance input atom

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import { Button } from "./components/atoms/button/button";
+import { Input } from "./components/atoms/input/input";
 
 const App = () => {
-  return <Button onClick={() => {}}>Test</Button>;
+  return (
+    <>
+    <Input onChange={()=> {}} placeholder="Gift URL" errorMessage="error"/>
+    <Button onClick={() => {}}>Test</Button>
+  </>
+  );
 };
 
 export default App;
